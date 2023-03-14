19de8a4428c3d2ea0919da0d6cbb93f962d6f03a
Avik Basu <ab93@users.noreply.github.com>
2023-03-13 20:22:05 -0400

    fix tests
    Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,11 +7,8 @@ redis_client = fakeredis.FakeStrictRedis(server=server, decode_responses=True)
 
 with patch("numaprom.redis.get_redis_client") as mock_get_redis_client:
     mock_get_redis_client.return_value = redis_client
-    with patch("numaprom.tools.set_aws_session") as mock_aws_session:
-        mock_aws_session.return_value = None
-
-        from numaprom.udf import window
-        from numaprom.udsink import train, train_rollout
+    from numaprom.udf import window
+    from numaprom.udsink import train, train_rollout
 
 
 __all__ = ["redis_client", "window", "train", "train_rollout"]
