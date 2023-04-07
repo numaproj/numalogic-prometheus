5d4c92c41fc8dfdf7e4ffdb58d3344e8893b4b24
Avik Basu <ab93@users.noreply.github.com>
2023-04-07 14:41:26 -0700

    Add exceptions for retry
    Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

--- numaprom/clients/redis.py
+++ numaprom/clients/redis.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from redis.cluster import RedisCluster
 from redis.backoff import ExponentialBackoff
+from redis.exceptions import RedisClusterException, RedisError
 from redis.retry import Retry
 
 from numaprom import get_logger
@@ -34,6 +35,10 @@ def get_redis_client(
     if not is_host_reachable(host, port):
         _LOGGER.error("Redis Cluster is unreachable after retries!")
 
-    retry = Retry(ExponentialBackoff(cap=2, base=1), 3)
+    retry = Retry(
+        ExponentialBackoff(cap=2, base=1),
+        3,
+        supported_errors=(ConnectionError, TimeoutError, RedisClusterException, RedisError),
+    )
     redis_client = RedisCluster(**redis_params, retry=retry)
     return redis_client
