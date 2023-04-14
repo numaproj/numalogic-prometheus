9ce0db021f71b7baf8bd25bab95251e4d8eb73b9
Avik Basu <3485425+ab93@users.noreply.github.com>
2023-04-14 14:35:16 -0700

    feat: support sentinel redis (#118)
    Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,7 @@ import fakeredis
 server = fakeredis.FakeServer()
 redis_client = fakeredis.FakeStrictRedis(server=server, decode_responses=True)
 
-with patch("numaprom.clients.redis.get_redis_client") as mock_get_redis_client:
+with patch("numaprom.clients.sentinel.get_redis_client") as mock_get_redis_client:
     mock_get_redis_client.return_value = redis_client
     from numaprom.udf import window
     from numaprom.udsink import train, train_rollout
