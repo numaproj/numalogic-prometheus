2f7c86296c8d43e890e6dd045b94690b5f45a0e3
Avik Basu <ab93@users.noreply.github.com>
2023-05-31 11:55:18 -0400

    fix: unit tests
    Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,12 +1,15 @@
 import unittest
 
-from numaprom.factory import HandlerFactory
+from pynumaflow.function import MultiProcServer
+from pynumaflow.sink import Sink
+
+from numaprom.factory import HandlerFactory, ServerFactory
 
 from numaprom.udf import metric_filter, Preprocess, postprocess, inference, threshold
 from numaprom.udsink import train, train_rollout
 
 
-class TestFactory(unittest.TestCase):
+class TestHandlerFactory(unittest.TestCase):
     def test_metric_filter(self):
         func = HandlerFactory.get_handler("metric_filter")
         self.assertEqual(func, metric_filter)
@@ -36,8 +39,29 @@ class TestFactory(unittest.TestCase):
         self.assertEqual(func, train_rollout)
 
     def test_invalid(self):
-        with self.assertRaises(NotImplementedError):
+        with self.assertRaises(ValueError):
             HandlerFactory.get_handler("Lionel Messi")
+
+
+class TestServerFactory(unittest.TestCase):
+    def test_inference(self):
+        func = HandlerFactory.get_handler("inference")
+        obj = ServerFactory.get_server("inference", func)
+        self.assertIsInstance(obj, MultiProcServer)
+
+    def test_train(self):
+        func = HandlerFactory.get_handler("train")
+        obj = ServerFactory.get_server("train", func)
+        self.assertIsInstance(obj, Sink)
+
+    def test_train_rollout(self):
+        func = HandlerFactory.get_handler("train_rollout")
+        obj = ServerFactory.get_server("train_rollout", func)
+        self.assertIsInstance(obj, Sink)
+
+    def test_invalid(self):
+        with self.assertRaises(ValueError):
+            ServerFactory.get_server("Some random string")
 
 
 if __name__ == "__main__":
