b07f85730bba23545efe9d65f25d6e83b2a6559e
s0nicboOm <i.kushalbatra@gmail.com>
2023-06-08 10:35:55 -0700

    fix: add numalogic logging
    Signed-off-by: s0nicboOm <i.kushalbatra@gmail.com>

--- numaprom/__init__.py
+++ numaprom/__init__.py
@@ -8,24 +8,28 @@ from numaprom._config import UnifiedConf, MetricConf, AppConf, DataConf
 def get_logger(name):
     formatter = logging.Formatter("%(asctime)s-%(levelname)s-%(message)s")
     logger = logging.getLogger(name)
+
     numalogic_logger = logging.getLogger("numalogic")
     pl_logger = logging.getLogger("pytorch_lightning")
+
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
+
     logger.addHandler(stream_handler)
+
     if os.getenv("DEBUG", False):
         logger.setLevel(logging.DEBUG)
         stream_handler.setLevel(logging.DEBUG)
         numalogic_logger.setLevel(logging.DEBUG)
-        pl_logger.setLevel(logging.ERROR)
     else:
         logger.setLevel(logging.INFO)
         stream_handler.setLevel(logging.INFO)
-        pl_logger.setLevel(logging.ERROR)
+        numalogic_logger.setLevel(logging.INFO)
 
+    pl_logger.setLevel(logging.ERROR)
     numalogic_logger.propagate = True
-    numalogic_logger.addHandler(stream_handler)
     pl_logger.propagate = False
+    numalogic_logger.addHandler(stream_handler)
     pl_logger.addHandler(stream_handler)
     return logger
 
