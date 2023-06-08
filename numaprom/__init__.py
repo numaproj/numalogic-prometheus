a40fea52b0a3a9a160d51cdb09c2dc5607ae528f
Kushal Batra <34571348+s0nicboOm@users.noreply.github.com>
2023-06-08 11:01:26 -0700

    Fix logs (#142)
    Signed-off-by: s0nicboOm <i.kushalbatra@gmail.com>

--- numaprom/__init__.py
+++ numaprom/__init__.py
@@ -7,20 +7,28 @@ from numaprom._config import UnifiedConf, MetricConf, AppConf, DataConf
 def get_logger(name):
     formatter = logging.Formatter("%(asctime)s-%(levelname)s-%(message)s")
     logger = logging.getLogger(name)
+
+    numalogic_logger = logging.getLogger("numalogic")
+    pl_logger = logging.getLogger("pytorch_lightning")
+
     stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
     if os.getenv("DEBUG", False):
         logger.setLevel(logging.DEBUG)
         stream_handler.setLevel(logging.DEBUG)
+        numalogic_logger.setLevel(logging.DEBUG)
     else:
         logger.setLevel(logging.INFO)
         stream_handler.setLevel(logging.INFO)
+        numalogic_logger.setLevel(logging.INFO)
 
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-
-    pl_logger = logging.getLogger("pytorch_lightning")
-    pl_logger.propagate = False
     pl_logger.setLevel(logging.ERROR)
+    numalogic_logger.propagate = True
+    pl_logger.propagate = False
+    numalogic_logger.addHandler(stream_handler)
     pl_logger.addHandler(stream_handler)
     return logger
 
